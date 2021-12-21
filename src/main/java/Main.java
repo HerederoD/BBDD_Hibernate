4d173c3c2c463f4ff9052fc8b0f01bbddb140d1c
TheAdri666 <adrianinfante66@gmail.com>
2021-12-21 23:10:57 +0100

    Fixed a typo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,16 +6,18 @@ import java.sql.SQLException;
 
 public class Main {
 
-    public static void main (String [] args) {
+    public static void main(String[] args) {
         try {
-            Controller controlador= new Controller();
-            //@TODO añada las llamadas a las funciones de la clase Controller.java necesarias
-            //para la creación de la daga y el mago.
-            Daga daga = createDaga("NombreGaga");
+            Controller controlador = new Controller();
+            // @TODO añada las llamadas a las funciones de la clase Controller.java
+            // necesarias
+            // para la creación de la daga y el mago.
+            Daga daga = createDaga("NombreDaga");
             Mago mago = createMago("NombreMago", daga);
-            if(daga!=null && mago!=null)
-                System.out.println("Se ha creado el mago "+mago.getnombre()+" de nivel 1 con la daga "+daga.getnombre());
-        } catch(SQLException e) {
+            if (daga != null && mago != null)
+                System.out.println(
+                        "Se ha creado el mago " + mago.getnombre() + " de nivel 1 con la daga " + daga.getnombre());
+        } catch (SQLException e) {
             System.err.println("Se ha producido un error en la conexión con la base de datos");
             e.printStackTrace();
         }
